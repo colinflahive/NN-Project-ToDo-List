1b4eaafd73f1440eb17dcda5cc89667d49af2be7
colinflahive <cflahive@hotmail.com>
2021-01-31 16:41:39 +0000

    http.server

--- todos/app.js
+++ todos/app.js
@@ -1,37 +1,47 @@
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos');
+const addForm = document.querySelector('.add');    
+const list = document.querySelector('.todos');      
+const search = document.querySelector('.search input');
 
-const generateTemplate = todo => {
-
+const generateTemplate = todo => {     
     const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${todo}</span>
         <i class="far fa-trash-alt delete"></i>
-      </li>
+      </li>     
     `;
-
-list.innerHTML += html;
-    
+    list.innerHTML += html;       
 };
 
-addForm.addEventListener('submit', e => {
-
-    e.preventDefault();
-    const todo = addForm.add.value.trim();
+// add todos
+addForm.addEventListener('submit', e => {     
+    e.preventDefault();     
+    const todo = addForm.add.value.trim();     
 
     if(todo.length){
-        generateTemplate(todo);
+        generateTemplate(todo);     
         addForm.reset();
     }
-
-
 });
 
 // delete todos
 list.addEventListener('click', e => {
-
     if(e.target.classList.contains('delete')){
     e.target.parentElement.remove();    
     }
+});
 
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+// kepup event
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
